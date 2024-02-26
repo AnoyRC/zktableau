b001a33511da7f32eb2118ba4652ca5814898a9a
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2024-02-27 03:08:56 +0530

    Proof Generation with ZK

--- app/page.js
+++ app/page.js
@@ -3,6 +3,10 @@
 import { Button, Input } from "@material-tailwind/react";
 import { useState } from "react";
 import { parse } from "csv-parse/browser/esm";
+import { BarretenbergBackend } from "@noir-lang/backend_barretenberg";
+import { Noir } from "@noir-lang/noir_js";
+import data_prove from "@/circuit/data_prove/target/data_prove.json";
+import { BigNumber, ethers } from "ethers";
 
 export default function Home() {
   const [file, setFile] = useState(null);
@@ -11,7 +15,7 @@ export default function Home() {
   const readCSV = async (file) => {
     const reader = new FileReader();
 
-    reader.onload = function (e) {
+    reader.onload = async function (e) {
       const content = e.target.result;
       const [keys, ...rest] = content
         .toString()
@@ -29,9 +33,12 @@ export default function Home() {
 
       console.log("CSV formedArr: ", formedArr);
 
-      formedArr.sort((a, b) => a.Founded - b.Founded);
+      // Remove items from formedArr if "Founded" property is not a number
+      const filteredArr = formedArr.filter((item) => !isNaN(item["Founded"]));
 
-      const numbers = formedArr.map((item) => parseInt(item["Founded"]));
+      filteredArr.sort((a, b) => a.Founded - b.Founded);
+
+      const numbers = filteredArr.map((item) => parseInt(item["Founded"]));
       console.log("CSV numbers: ", numbers);
 
       const uniqueNumbers = [...new Set(numbers)];
@@ -54,6 +61,67 @@ export default function Home() {
       }
 
       console.log("CSV classes:", classes);
+
+      const modifiedArr = filteredArr.map((item) => {
+        const founded = parseInt(item["Founded"]);
+        const classIndex = classes.findIndex(
+          (item) => founded >= item.lowerLimit && founded <= item.upperLimit
+        );
+
+        if (classIndex === -1) {
+          return {
+            input: null,
+            lowerLimit: "N/A",
+            upperLimit: "N/A",
+          };
+        }
+        const { lowerLimit, upperLimit } = classes[classIndex];
+
+        return {
+          input: founded,
+          lowerLimit,
+          upperLimit,
+        };
+      });
+
+      console.log("CSV modifiedArr:", modifiedArr);
+
+      const backend = new BarretenbergBackend(data_prove, {
+        threads: navigator.hardwareConcurrency,
+      });
+      const noir = new Noir(data_prove, backend);
+
+      const proofArray = [];
+      for (const item of modifiedArr) {
+        const { input, lowerLimit, upperLimit } = item;
+        if (input === null) {
+          proofArray.push({
+            lowerLimit,
+            upperLimit,
+            proof: null,
+          });
+        } else {
+          const inputs = {
+            min: ethers.utils.hexZeroPad(ethers.utils.hexlify(lowerLimit), 32),
+            max: ethers.utils.hexZeroPad(ethers.utils.hexlify(upperLimit), 32),
+            data: ethers.utils.hexZeroPad(ethers.utils.hexlify(input), 32),
+          };
+
+          try {
+            const proof = await noir.generateFinalProof(inputs);
+
+            proofArray.push({
+              lowerLimit,
+              upperLimit,
+              proof: ethers.utils.hexlify(proof.proof),
+            });
+          } catch (error) {
+            console.log(lowerLimit, upperLimit, input);
+          }
+        }
+      }
+
+      console.log("CSV proofArray:", proofArray);
     };
 
     reader.readAsText(file);
